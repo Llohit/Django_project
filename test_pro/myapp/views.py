d90c69679e76cb7ea609b20c27067e6b15ee0090
lohit.poojary <lohit.poojary@cradlepoint.com>
2024-12-25 12:59:18 +0530

    Local storage

--- test_pro/myapp/views.py
+++ test_pro/myapp/views.py
@@ -6,17 +6,28 @@ from rest_framework import status
 import uuid
 
 class Sample(ViewSet):
-
+    account_ids = []
     serializer_class= SampleSerializer
+
     def list(self,request):
-        return Response("No database to store yet!")
+        if len(self.account_ids):
+            acc_id_st = ""
+            for acc_id in self.account_ids:
+                acc_id_st += str(acc_id) + ","
+            acc_id_st = acc_id_st[:-1]
+
+            return Response(f"Account id's added till now are {acc_id_st}")
+
+        return Response(f"No account id's added till now!")
+
     def create(self,request):
         serializer = self.serializer_class(data= request.data)
         if(serializer.is_valid()):
             data=serializer.validated_data
-            print(data)
             unique_uuid=uuid.uuid4()
             data['uid']=unique_uuid
+            account_id=data['account']
+            self.account_ids.append(account_id)
             return Response(data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
