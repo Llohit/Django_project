e40012f247813c8caa651cd72eb72dd3bf95a904
lohit.poojary <lohit.poojary@cradlepoint.com>
2025-01-11 01:24:04 +0530

    Sqlite db use

--- test_pro/myapp/urls.py
+++ test_pro/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter(trailing_slash=False)
+router = routers.DefaultRouter()
 router.register(r'/upload', views.Sample, basename="upload")
 urlpatterns = [
     path("api/lohit",include(router.urls)),
--- test_pro/myapp/views.py
+++ test_pro/myapp/views.py
@@ -3,19 +3,17 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from .serializers import SampleSerializer
 from rest_framework import status
+from .models import Account
 import uuid
 
 class Sample(ViewSet):
-    account_details = {}
     serializer_class= SampleSerializer
 
     def list(self,request):
-        if len(self.account_details):
-            #GET all the list from db
-            account_data = self.account_details.values()
-            serializer = self.serializer_class(account_data, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(f"No account id's added till now!")
+        #GET all the list from db
+        account_data = Account.objects.all()
+        serializer = self.serializer_class(account_data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self,request):
         serializer = self.serializer_class(data= request.data)
@@ -26,24 +24,26 @@ class Sample(ViewSet):
             unique_uuid=uuid.uuid4()
             data['id']=unique_uuid
             account_id=data['account']
+            print(data,"Hi")
             #Store in db
-            self.account_details[account_id] = data
+            Account.objects.create(**data)
             return Response(data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self,request,pk):
-        account_id = int(pk)
-        if account_id not in self.account_details:
-            return Response({f"Account id:{account_id} is not found"}, status=status.HTTP_403_FORBIDDEN)
-        account_details = self.account_details[account_id]
-        serializer = self.serializer_class(account_details)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        try:
+            account_details = Account.objects.get(pk=pk)
+            serializer = self.serializer_class(account_details)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Account.DoesNotExist:
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
-    def delete(self,pk):
-        account_id = int(pk)
-        if account_id not in self.account_details:
-            return Response({f"Account id:{account_id} is not found"}, status=status.HTTP_403_FORBIDDEN)
-        self.account_details.pop(account_id)
-        return Response(status= status.HTTP_204_NO_CONTENT)
+    def destroy(self, request, pk):
+        try:
+            account_details = Account.objects.get(pk=pk)
+            account_details.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Account.DoesNotExist:
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
 # Create your views here.
